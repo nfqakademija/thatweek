5f7e2e2823bab6d15be937f6b603a4fd35369df5
Martynas Treinys <martynas7928@gmail.com>
2017-11-06 13:28:28 +0200

    Orders history added

---
+++ src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Service\UserHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\User;
+
+/**
+ * Class ProfileController
+ * @package AppBundle\Controller
+ * @Route("/profile")
+ */
+class ProfileController extends Controller
+{
+    /**
+     * @Route("/orders", name="profile.show.orders")
+     */
+    public function showOrdersAction(UserHandler $userHandler)
+    {
+        $user = $this->getUser()->getEntity();
+
+        $orders = $userHandler->getOrders($user);
+        return $this->render('AppBundle:Profile:order_history.html.twig', array(
+            'orders' => $orders
+        ));
+    }
+
+}
